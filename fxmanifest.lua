97df19cc57e0b9f54c0f40c6c103cee1100e652f
CRL SERVICES <gohlkecarl2101@gmail.com>
2023-03-23 10:24:01 +0100

    ESX Ready

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'CRL Farming'
+lua54 'yes'
+
+version '1.0'
+
+dependency 'es_extended'
+
+
+client_scripts {
+    'config.lua',
+    'client/client.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server/server.lua'
+}
---
+++ server/server.lua
@@ -0,0 +1,8 @@
+ESX = nil
+
+
+if Config.GetSharedObjectfunction == true then
+    ESX = exports["es_extended"]:getSharedObject()
+else
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+end
